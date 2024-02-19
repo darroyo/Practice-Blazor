e50d41c6f3772f0a8e9bbf3211d787b82bd1c340
Daniel Arroyo <daniel.arroyo.cortes@avanade.com>
2024-02-19 10:45:52 +0100

    no message

--- 2 - Practice Blazor Web Assembly/WebAssembly/Pages/EmployeeDetails.razor.cs
+++ 2 - Practice Blazor Web Assembly/WebAssembly/Pages/EmployeeDetails.razor.cs
@@ -1,9 +1,10 @@
 ﻿using BethanysPieShopHRM.Shared.Domain;
+using BethanysPieShopHRM.Shared.Model;
 using Microsoft.AspNetCore.Components;
 using WebAssembly.Models;
 using WebAssembly.Services;
 
-namespace WebAssembly.Pages 
+namespace WebAssembly.Pages
 {
     public partial class EmployeeDetails
     {
@@ -12,10 +13,18 @@ namespace WebAssembly.Pages
         [Parameter]
         public string EmployeeId { get; set; }
         public Employee Employee { get; set; } = null;
+        public List<Marker> MapMarkers { get; set; } = new List<Marker>();
 
         protected override async Task OnInitializedAsync()
         {
             Employee = (await EmployeeDataService.GetEmployeeDetails(Int32.Parse(EmployeeId)));
+            if (Employee.Longitude.HasValue && Employee.Latitude.HasValue)
+            {
+                MapMarkers = new List<Marker>
+                {
+                    new Marker{Description = $"{Employee.FirstName} {Employee.LastName}",  ShowPopup = false, X = Employee.Longitude.Value, Y = Employee.Latitude.Value}
+                };
+            }
         }
     }
 }
