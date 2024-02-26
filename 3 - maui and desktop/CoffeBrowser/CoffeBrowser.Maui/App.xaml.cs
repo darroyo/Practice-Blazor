1309aaf080c02a2697ca981d1657078f9c93102c
Daniel Arroyo <daniel.arroyo.cortes@avanade.com>
2024-02-26 09:36:16 +0100

    no message

---
+++ 3 - maui and desktop/CoffeBrowser/CoffeBrowser.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CoffeBrowser.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
